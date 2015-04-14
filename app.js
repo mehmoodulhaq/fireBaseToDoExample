b714ec66d367c06a6ed9a82d426af143db936165
Mahmoodulhaq <sweet.sunrise90@gmail.com>
2015-04-14 16:49:53 +0500

    Added read add and remove functionality.

--- app.js
+++ app.js
@@ -1,22 +1,67 @@
-
+//https://mfirstapp.firebaseio.com/
 
 (function () {
     'use strict';
     angular.module('toDoApp',['firebase'])
-        .factory('toDoService',[function(){
+        .factory('toDoService',['$timeout','$q',function($timeout,$q){
+            var mainRef = new Firebase('https://mfirstapp.firebaseio.com/');
+            var toDoListRef = mainRef.child('toDo');
+
             return {
                 getToDoList:function(){
                     var toDoList =[];
+                    toDoListRef.on('child_added', function(dataSnapshot) {
+                        var items = dataSnapshot.val();
+                        var itemsKey = dataSnapshot.key();
 
-                    for(var i = 1 ;  i < 11 ; i++){
-                        toDoList.push({title:i})
-                    }
+                        $timeout(function(){
+                            toDoList.push({
+                                $id:itemsKey,
+                                title: items
+                            });
+                        });
+                    });
+
                     return toDoList;
+                },
+                addItem: function ( title ) {
+                    toDoListRef.push( title );
+                },
+                removeItem: function (item) {
+                    var promise = $q.defer();
+
+                    toDoListRef.child(item.$id)
+                        .remove(function (err) {
+                            if(err) {
+                                console.log(err);
+                                promise.reject(err)
+                            }
+                            else{
+                                console.log('Item removed!');
+                                promise.resolve('Item removed')
+                            }
+                        });
+
+                    return promise.promise;
                 }
+
             }
         }])
         .controller('toDoCtrl',['$scope','toDoService',function($scope,toDoService){
             $scope.myList = toDoService.getToDoList();
+            $scope.addItem = function () {
+                toDoService.addItem( $scope.title );
+                $scope.title='';
+            };
+            $scope.removeItem = function (item,index) {
+                toDoService.removeItem(item)
+                    .then(function(res){
+                        $scope.myList.splice(index,1);
+                    })
+                    .catch(function (err) {
+                        console.log(err)
+                    })
+            }
 
         }])
 
